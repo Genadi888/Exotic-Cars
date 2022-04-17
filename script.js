08c7b9397f61c8737b8002d179aa1f2f64975c09
Genadi888 <genadi.fidanov@abv.bg>
2022-04-17 10:52:29 +0300

    added carousel and changed navbar drop-down behavior

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let collapseButtonClicked = false;
-let navbarUlOriginalMarginLeft = null;
 let clicked = false;
 
 function collapseButtonClickFunction() {
 	const ul = document.getElementsByClassName('navbar-nav')[0];
-	navbarUlOriginalMarginLeft = ul.style.marginLeft;
 	
 	if (!clicked) {
 		ul.style.marginLeft = '0px';
@@ -16,15 +13,11 @@ function collapseButtonClickFunction() {
 }
 
 function getUlBack(ul) {
-	let nav = document.getElementsByTagName('nav')[0];
-	const initialHeight = nav.offsetHeight;
-	
-	setInterval(() => {
-		nav = document.getElementsByTagName('nav')[0];
+	const nav = document.getElementsByTagName('nav')[0];
 
+	setInterval(() => {
 		if (nav.offsetHeight > 80) {
 			ul.style.marginLeft = '0%'
-			// console.log(ul.style.marginLeft);
 		} else if (nav.offsetHeight >= 66 && nav.offsetHeight <= 80) {
 			ul.style.marginLeft = '55%';
 		}
