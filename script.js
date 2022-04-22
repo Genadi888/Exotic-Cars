83b465da5cf387c4202b90cebbf27d413a0fc0a2
Genadi888 <genadi.fidanov@abv.bg>
2022-04-22 15:41:27 +0300

    added fade-in animation to the goal-div

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let clicked = false;
 
 function collapseButtonClickFunction() {
 	const ul = document.getElementsByClassName('navbar-nav')[0];
-	
+
 	if (!clicked) {
 		ul.style.marginLeft = '0px';
 		clicked = true;
@@ -23,3 +23,42 @@ function getUlBack(ul) {
 		}
 	}, 10);
 }
+
+
+//? Below is the code for the "goal-div" scroll fade-in animation
+
+
+const scrollOffset = 100;
+
+const scrollElement = document.querySelectorAll(".goal-div")[0];
+console.log(scrollElement);
+
+const elementInView = (el, offset = 0) => {
+	const elementTop = el.getBoundingClientRect().top;
+
+	return (
+		elementTop <=
+		((window.innerHeight || document.documentElement.clientHeight) - offset)
+	);
+};
+
+const displayScrollElement = () => {
+	scrollElement.classList.add('scrolled');
+}
+
+const hideScrollElement = () => {
+	scrollElement.classList.remove('scrolled');
+	scrollElement.classList.add('fade-out');
+}
+
+const handleScrollAnimation = () => {
+	if (elementInView(scrollElement, scrollOffset)) {
+		displayScrollElement();
+	} else {
+		hideScrollElement();
+	}
+}
+
+window.addEventListener('scroll', () => {
+	handleScrollAnimation();
+})
