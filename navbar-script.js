e2f28809bf1f487ee6f88bd4918a1ed90a7f5d68
Genadi888 <genadi.fidanov@abv.bg>
2022-04-22 21:57:19 +0300

    added "car-pictures" page and made the navbar script global

---
+++ navbar-script.js
@@ -0,0 +1,29 @@
+//? we use this file for multiple pages' navbars
+
+'use strict'
+
+let clicked = false;
+
+function collapseButtonClickFunction() {
+	const ul = document.getElementsByClassName('navbar-nav')[0];
+
+	if (!clicked) {
+		ul.style.marginLeft = '0px';
+		clicked = true;
+	} else {
+		getUlBack(ul);
+		clicked = false;
+	}
+}
+
+function getUlBack(ul) {
+	const nav = document.getElementsByTagName('nav')[0];
+
+	setInterval(() => {
+		if (nav.offsetHeight > 80) {
+			ul.style.marginLeft = '0%'
+		} else if (nav.offsetHeight >= 66 && nav.offsetHeight <= 80) {
+			ul.style.marginLeft = '55%';
+		}
+	}, 10);
+}
--- scroll-anim.js
+++ scroll-anim.js
@@ -1,32 +1,3 @@
-'use strict'
-
-let clicked = false;
-
-function collapseButtonClickFunction() {
-	const ul = document.getElementsByClassName('navbar-nav')[0];
-
-	if (!clicked) {
-		ul.style.marginLeft = '0px';
-		clicked = true;
-	} else {
-		getUlBack(ul);
-		clicked = false;
-	}
-}
-
-function getUlBack(ul) {
-	const nav = document.getElementsByTagName('nav')[0];
-
-	setInterval(() => {
-		if (nav.offsetHeight > 80) {
-			ul.style.marginLeft = '0%'
-		} else if (nav.offsetHeight >= 66 && nav.offsetHeight <= 80) {
-			ul.style.marginLeft = '55%';
-		}
-	}, 10);
-}
-
-
 //? Below is the code for the "goal-div" scroll fade-in animation
 
 
