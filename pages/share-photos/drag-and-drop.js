604c1a5943b49bd42f2eb5cfd2b526a9002f429d
Genadi888 <genadi.fidanov@abv.bg>
2022-04-30 21:28:01 +0300

    added behavior to the "drag-and-drop" container

--- pages/share-photos/drag-and-drop.js
+++ pages/share-photos/drag-and-drop.js
@@ -1,7 +1,18 @@
 const inputElement = document.querySelector('.drop-zone__input');
 const dropZoneElement = inputElement.closest('.drop-zone');
 
+dropZoneElement.addEventListener('click', el => {
+	inputElement.click();
+});
+
+inputElement.addEventListener('change', el => {
+	if (inputElement.files.length) {
+		updateThumbnail(dropZoneElement, inputElement.files);
+	}
+});
+
 dropZoneElement.addEventListener('dragover', el => {
+	el.preventDefault();
 	dropZoneElement.classList.add('drop-zone--over');
 });
 
@@ -11,3 +22,61 @@ dropZoneElement.addEventListener('dragover', el => {
 		dropZoneElement.classList.remove('drop-zone--over');
 	});
 });
+
+dropZoneElement.addEventListener('drop', el => {
+	el.preventDefault();
+
+	let filesArr = Array.from(el.dataTransfer.files);
+	// for (const file of el.dataTransfer.files) {
+	// 	filesArr.push(file)
+	// }
+	filesArr = filesArr.filter(file => file.type.startsWith('image/'));
+	console.log(filesArr);
+	
+	if (filesArr.length) {
+		console.log(inputElement.files);
+		new FileList()
+		inputElement.files = filesArr;
+		updateThumbnail(dropZoneElement, filesArr);
+	}
+
+	dropZoneElement.classList.remove('drop-zone--over');
+});
+
+/** 
+	* @param {HTMLElement} dropZoneElement
+	* @param {File} firstFile
+*/
+
+function updateThumbnail(dropZoneElement, filesArr) {
+	const firstFile = filesArr[0];
+	let thumbnailElement = dropZoneElement.querySelector('.drop-zone__thumb');
+
+	if (dropZoneElement.querySelector('.drop-zone__prompt')) {
+		dropZoneElement.querySelector('.drop-zone__prompt').remove();
+	}
+
+	//? If there's no a thumbnail element, It needs to be created
+	if (!thumbnailElement) {
+		thumbnailElement = document.createElement('div');
+		thumbnailElement.classList.add('drop-zone__thumb');
+		dropZoneElement.appendChild(thumbnailElement)
+	}
+
+	let label = '';
+	filesArr.forEach(file => label += file.name + ', ');
+	label = label.slice(0, -2);
+	thumbnailElement.dataset.label = label;
+
+	//? Show thumbnail for image files only
+	if (firstFile.type.startsWith('image/')) {
+		const reader = new FileReader();
+
+		reader.readAsDataURL(firstFile);
+		reader.onload = () => {
+			thumbnailElement.style.backgroundImage = `url(${reader.result})`;
+		}
+	} else {
+		thumbnailElement.style.backgroundImage = null;
+	}
+}
