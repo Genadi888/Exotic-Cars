e24a18438148ae2604ff3e75529df5564018b735
Genadi888 <genadi.fidanov@abv.bg>
2022-06-15 22:50:51 +0300

    improved the "scroll-to-top" button (could add slide-left animation)

--- pages/car-pictures/scroll-to-top.js
+++ pages/car-pictures/scroll-to-top.js
@@ -1,3 +1,20 @@
+const scrollToTopBtn = document.getElementById('go-to-top-link');
+let allowedToScroll = false;
+
+setInterval(() => {
+	if (window.scrollY > 1000) {
+		scrollToTopBtn.style.opacity = 1;
+		scrollToTopBtn.style.cursor = 'pointer';
+		allowedToScroll = true;
+	} else {
+		scrollToTopBtn.style.opacity = 0;
+		scrollToTopBtn.style.cursor = 'unset';
+		allowedToScroll = false;
+	}
+}, 500);
+
 function scrollToTop() {
-	window.scrollTo({ top: 0, behavior: 'smooth' });
+	if (allowedToScroll) {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
 }
--- scroll-anim.js
+++ scroll-anim.js
@@ -1,20 +1,18 @@
 //? Below is the code for the "goal-div" scroll fade-in animation
 
 
-const observerOptions = {
+const observer = new IntersectionObserver(observerCallback, {
 	root: null,
 	rootMargin: "0px",
 	threshold: 0.8
-};
+});
 
-const observer = new IntersectionObserver(observerCallback, observerOptions);
-
-const fadeElms = document.querySelectorAll('.goal-div');
-observer.observe(fadeElms[0]);
+const fadeEl = document.querySelector('#goal-div');
+observer.observe(fadeEl);
 
 function observerCallback(entries) {
 	const goalDiv = entries[0];
-	
+
 	if (goalDiv.isIntersecting) {
 		//? fade in observed element that are in view
 		goalDiv.target.classList.replace('fadeOut', 'fadeIn');
