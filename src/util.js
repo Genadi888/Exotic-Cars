4c16334e117394cfb30eb4e97a99e5f29dc36229
Genadi888 <genadi.fidanov@abv.bg>
2022-10-23 21:44:25 +0300

    added working carousel to home page, still need to add a drop-down nav menu

--- src/util.js
+++ src/util.js
@@ -1,29 +1,5 @@
 'use strict'
 
-export function startObserving(nestedShadowRoot) {
-	const observer = new IntersectionObserver(observerCallback, {
-		root: null,
-		rootMargin: "0px",
-		threshold: 0.8
-	});
-	
-	const fadeEl = nestedShadowRoot.querySelector('#goal-div');
-	// console.log(fadeEl)
-	observer.observe(fadeEl);
-	
-	function observerCallback(entries) {
-		const goalDiv = entries[0];
-	
-		if (goalDiv.isIntersecting) {
-			//? fade in observed element that are in view
-			goalDiv.target.classList.replace('fadeOut', 'fadeIn');
-		} else {
-			//? fade out observed element that are not in view
-			goalDiv.target.classList.replace('fadeIn', 'fadeOut');
-		}
-	}
-}
-
 let clickedNavBtn = false;
 
 export function collapseButtonClickFunction(topShadowRoot) {
