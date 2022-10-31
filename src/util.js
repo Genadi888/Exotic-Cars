891aeab10bdbe1043fcd5642dee59b06865bbc30
Genadi888 <genadi.fidanov@abv.bg>
2022-10-31 11:57:49 +0200

    added drop-down menu to the home page

--- src/util.js
+++ src/util.js
@@ -1,41 +1,23 @@
-'use strict'
-
 let clickedNavBtn = false;
 
-export function collapseButtonClickFunction(topShadowRoot) {
-	const ul = topShadowRoot.querySelector('.navbar-nav');
-	const socialMediaLi = topShadowRoot.querySelector('#navbarNav>ul>li:nth-child(4)');
-
+export function collapseButtonClickFunction(topShadowRoot, ev) {
+	ev.stopPropagation();
+	const menuContainer = topShadowRoot.querySelector('#drop-down-menu-container');
+	const button = ev.currentTarget;
+	
 	if (!clickedNavBtn) {
-		ul.style.marginLeft = '0px';
-
-		socialMediaLi.style.marginLeft = '0px';
-		socialMediaLi.style.paddingLeft = '0px';
-		socialMediaLi.style.paddingTop = '8px';
-
+		menuContainer.style.transform = 'translateY(100%)';
 		clickedNavBtn = true;
+
+		button.disabled = true;
 	} else {
-		getUlBack(ul, topShadowRoot);
+		menuContainer.style.transform = 'translateY(-100%)';
 		clickedNavBtn = false;
+
+		button.disabled = true;
 	}
-}
-
-function getUlBack(ul, nestedShadowRoot) {
-	const nav = nestedShadowRoot.querySelector('nav');
-	const socialMediaLi = nestedShadowRoot.querySelector('#navbarNav>ul>li:nth-child(4)');
-
-	setInterval(() => {
-		// console.log(nav.offsetHeight);
-		if (nav.offsetHeight > 128) {
-			ul.style.marginLeft = '0%';
-
-			socialMediaLi.style.marginLeft = '0px';
-			socialMediaLi.style.paddingLeft = '0px';
-			socialMediaLi.style.paddingTop = '8px';
-		} else if (nav.offsetHeight >= 66 && nav.offsetHeight <= 80) {
-			socialMediaLi.style.marginLeft = '20px';
-			socialMediaLi.style.paddingLeft = '0px';
-			socialMediaLi.style.paddingTop = '0px';
-		}
-	}, 10);
+	
+	menuContainer.addEventListener('transitionend', () => {
+		button.removeAttribute('disabled');
+	})
 }
--- src/views/layout.js
+++ src/views/layout.js
@@ -4,18 +4,45 @@ import { collapseButtonClickFunction } from "../util.js";
 export const layoutTemplate = (body, topShadowRoot, onLogout, user) => html`
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-		crossorigin="anonymous"></script> -->
 	<link rel="stylesheet" href="/css/navbar-and-footer.css">
 
+	<div id="drop-down-menu-container">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="/share-photos">SHARE YOUR CAR PHOTOS!</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/about-us">ABOUT US</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/showrooms">SHOWROOMS</a>
+			</li>
+			<li class="nav-item">
+				<ul class="social-media-links">
+					<li>
+						<a href="https://www.facebook.com" target="_blank"><img src="/images/facebook.svg" alt="facebook"></a>
+					</li>
+					<li>
+						<a href="https://www.instagram.com" target="_blank"><img src="/images/instagram.svg"
+								alt="instagram"></a>
+					</li>
+					<li>
+						<a href="https://twitter.com" target="_blank"><img src="/images/twitter.svg" alt="twitter"></a>
+					</li>
+					<li>
+						<a href="https://www.youtube.com" target="_blank"><img src="/images/youtube.svg" alt="youtube"></a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
 	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
 		<div class="container-fluid">
 			<a href="/">
 				<img width="50px" height="50px" src="/images/site-logo.png" alt="sports car">
 			</a>
 			<a class="navbar-brand" href="/">Exotic Cars</a>
-			<button @click=${() => collapseButtonClickFunction(topShadowRoot)} id="collapse-button" class="navbar-toggler" type="button"
+			<button @click=${ev => collapseButtonClickFunction(topShadowRoot, ev)} id="collapse-button" class="navbar-toggler" type="button"
 				data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
 				aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
