42b739a329cea8e137ad0a632e204ee7a950fe3f
Genadi888 <genadi.fidanov@abv.bg>
2022-12-19 22:44:55 +0200

    added register view

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { aboutUsView } from "./views/about-us.js";
 import { carPicturesView } from "./views/car-pictures.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
+import { registerView } from "./views/register.js";
 import { sharePhotosView } from "./views/share-photos.js";
 import { showroomsView } from "./views/showrooms.js";
 
@@ -27,4 +28,5 @@ page('/about-us', aboutUsView);
 page('/showrooms', showroomsView);
 page('/share-photos', sharePhotosView);
 page('/login', loginView);
+page('/register', registerView);
 page.start();
---
+++ src/views/register.js
@@ -0,0 +1,31 @@
+import { html } from "../lib/lit-html.js";
+
+const registerTemplate = () => html`
+	<link rel="stylesheet" href="/css/register.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+	<main>
+		<form>
+			<h1>Register</h1>
+			<div class="inputs">
+				<label for="username">Username:</label>
+				<input class="form-control" type="text" name="username" id="username">
+				<label for="password">Password:</label>
+				<input class="form-control" type="password" name="password" id="password">
+				<label for="password">E-mail:</label>
+				<input class="form-control" type="email" name="email" id="email">
+				<div class="form-check">
+					<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+					<label class="form-check-label" for="flexCheckChecked">
+						Remember me
+					</label>
+				</div>
+			</div>
+		</form>
+	</main>
+`
+
+export function registerView(ctx) {
+	ctx.render(registerTemplate());
+}
